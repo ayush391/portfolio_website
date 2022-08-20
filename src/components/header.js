1a5f884a883b2afaf2810e82307441e9c4fbe16e
ayush391 <stringakaak@gmail.com>
2022-08-20 16:30:17 +0530

    added resposive font size

--- src/components/header.js
+++ src/components/header.js
@@ -12,14 +12,19 @@ export const Header = () => {
 
 
         </div>
-          <div class='dp-container'>
-            <img class='rounded dp' src={DP}></img>
+        <div class='dp-container'>
+          <img class='rounded dp' src={DP}></img>
 
           <div class='dp-text'>
             <h2 class='name'>Ayush Kapoor</h2>
             <h4 class='subtitle'>Web Developer</h4>
           </div>
+          <div class='header-text'>
+            <h1>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </h1>
           </div>
+        </div>
 
       </section>
 
